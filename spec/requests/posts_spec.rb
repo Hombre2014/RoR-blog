14c84657d81c0bb797739834d2a5a3e5f665548e
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-31 21:40:40 +0200

    Clen linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET #index' 
-    before(:each) { get user_posts_path(user_id: 1) }
+  describe 'GET #index'
+  before(:each) { get user_posts_path(user_id: 1) }
 
-    it 'GET requests is success' do
-      expect(response).to have_http_status(:ok)
-    end
+  it 'GET requests is success' do
+    expect(response).to have_http_status(:ok)
+  end
 
-    it 'renders a correct template - index' do
-      expect(response).to render_template(:index)
-    end
+  it 'renders a correct template - index' do
+    expect(response).to render_template(:index)
+  end
 
-    it 'shows the text "All the posts from a user"' do
-      expect(response.body).to include('All the posts from a user')
-    end
+  it 'shows the text "All the posts from a user"' do
+    expect(response.body).to include('All the posts from a user')
   end
 
   describe 'GET #show' do
