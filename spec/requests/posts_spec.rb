82759556edc51bd449b988aff2963aad817fb1ef
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-31 21:36:46 +0200

    Add requests test for users and posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' 
+    before(:each) { get user_posts_path(user_id: 1) }
+
+    it 'GET requests is success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders a correct template - index' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'shows the text "All the posts from a user"' do
+      expect(response.body).to include('All the posts from a user')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) { get user_post_path(user_id: 1, id: 1) }
+
+    it 'GET requests is success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders a correct template - show' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'shows the text "Single post page - Title"' do
+      expect(response.body).to include('Single post page - Title')
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users controller', type: :request do
-  describe 'GET /index' do
+  describe 'GET #index' do
     before(:each) { get users_path }
 
     it 'GET requests is a success' do
@@ -14,6 +14,22 @@ RSpec.describe 'Users controller', type: :request do
 
     it 'includes the text "Welcome to the Ruby on Rails Blog"' do
       expect(response.body).to include('Welcome to the Ruby on Rails Blog')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) { get user_path(id: 1) }
+
+    it 'GET requests is success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template - show' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the test "Single user"' do
+      expect(response.body).to include('Single user')
     end
   end
 end
