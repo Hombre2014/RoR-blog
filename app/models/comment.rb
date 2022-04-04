d9802f46ed6e6ea136cdcc404acdd75332a0d0d2
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-04-04 17:26:31 +0200

    Modify models and assosiations and add methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
-  belongs_to :user
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+    post.save
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+    post.save
+  end
 end
