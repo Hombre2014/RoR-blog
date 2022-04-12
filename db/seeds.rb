e133cbf1132da3711227d6df25d4067f926f0cf0
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-04-12 22:29:16 +0200

    Change the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-image = 'https://images.unsplash.com/photo-1648974299612-679d6fb46816?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-user1 = User.create!( name: 'Tom', photo: image, bio:'Programer', posts_counter: 0, email: 'tom@example.com', password: 'tom123' )
-user2 = User.create!( name: 'Ed', photo: image, bio: 'Artist', posts_counter: 0, email: 'ed@example.com', password: 'ed1234' )
-user3 = User.create!( name: 'King', photo: image, bio: 'Dancer', posts_counter: 0, email: 'king@example.com', password: 'king123' )
-user4 = User.create!( name: 'David', photo: image, bio: 'Painter', posts_counter: 0, email: 'david@example.com', password: 'david123')
+# user1 = User.create!( name: 'Tom', photo: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', bio:'Programer', posts_counter: 0, email: 'tom@example.com', password: 'tom123' )
+# user2 = User.create!( name: 'Dick', photo: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', bio: 'Artist', posts_counter: 0, email: 'ed@example.com', password: 'dick1234' )
+# user3 = User.create!( name: 'Anna', photo: 'https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80', bio: 'Dancer', posts_counter: 0, email: 'king@example.com', password: 'anna123' )
+# user4 = User.create!( name: 'David', photo: 'https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', bio: 'Painter', posts_counter: 0, email: 'david@example.com', password: 'david123')
 
-post1 = user1.posts.create!( title: 'programmer', text: 'Iam Programmer I have life', likes_counter: 0, comments_counter: 0)
-post2 = user2.posts.create!( title: 'skyDiving!!', text: 'Going sky diving this weekend wanna come?', likes_counter: 0, comments_counter: 0) 
+# post1 = user1.posts.create!( title: 'programmer', text: 'Iam Programmer I have life', likes_counter: 0, comments_counter: 0)
+# post2 = user2.posts.create!( title: 'skyDiving!!', text: 'Going sky diving this weekend wanna come?', likes_counter: 0, comments_counter: 0) 
 
-4.times {
-  |i| 
-  post1 = Post.create(author: user1, title: 'Hello', text: "This is my no.#{i} post")
-}
+# 5.times {
+#   |i| 
+#   post1 = Post.create(author: user1, title: 'Hello', text: "This is my no.#{i} post")
+# }
 
-6.times {
-  |i|
-  Comment.create(post: post1, author: user2, text: "Hi Tom!#{i}" )
-}
+# 6.times {
+#   |i|
+#   Comment.create(post: post1, author: user2, text: "Hi Tom!#{i}" )
+# }
