47a35f0f1c1d6bc8dfe67ffe1e7dd2a4d12d9d9c
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-04-13 18:31:05 +0200

    Add cancan and implement authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
@@ -16,12 +18,20 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
 
     if @post.save
-      flash[:notice] = 'Post have been created successfully.'
+      flash[:notice] = 'The post have been created successfully.'
       redirect_to "#{users_path}/#{current_user.id}"
     else
       flash[:alert] = 'Adding a new post failed.'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @author = @post.author
+    @author.posts_counter -= 1
+    @post.destroy!
+    redirect_to user_posts_path(id: @author.id), notice: 'The post have been deleted successfully!'
   end
 
   private
